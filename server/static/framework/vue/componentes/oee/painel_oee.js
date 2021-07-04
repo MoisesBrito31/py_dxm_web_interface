d18f27557cef9ccb8ebce3db91bf4a909f1bf339
MoisesBrito31 <moisesbritofuentes@gmail.com>
2021-07-03 23:46:02 -0300

    fim do oee/linha

--- server/static/framework/vue/componentes/oee/painel_oee.js
+++ server/static/framework/vue/componentes/oee/painel_oee.js
@@ -1,9 +1,8 @@
 Vue.component('painel_oee',{
     delimiters: ["[[", "]]"],
     template: `
-    <div class="col-auto">
-            <div class="bg-light text-center border rounded mb-5 p-2" style="min-width:150px; max-width:250px;" v-on:click="acessaLinha" >
-            <div :class="fundo_bg">
+    <div >
+            <div :class="fundo_bg" style="min-width:150px; max-width:250px;" v-on:click="acessaLinha" >
                 <h3 id="l {{ l.id }}">[[linha.nome]]</h3>
                 <gage :anima=true ref="gage" :id="linha.id" title="OEE" symbol="%" ></gage>
                 <h4>
@@ -11,9 +10,8 @@ Vue.component('painel_oee',{
                         <span :class="texto_bg">[[linha.estado]]</span>
                     </p>    
                 </h4>  
-            </div>          
-            </div>
-        </div>
+            </div>                
+    </div>
     `,
     created(){
         setInterval(() => {
@@ -22,7 +20,7 @@ Vue.component('painel_oee',{
     },
     data(){
         return{
-            fundo_bg:"bg-light",
+            fundo_bg:"bg-light text-center border rounded mb-5 p-2",
             texto_bg:"text-dark",
             linha: {'id':this.id,'nome':this.title,'oee':this.oee,'estado':this.status},
         }
@@ -54,16 +52,16 @@ Vue.component('painel_oee',{
         pisca(){
             if(this.linha.estado=="Parado"){
                 this.$refs.gage.emit_alerta(true)
-                if(this.fundo_bg=="bg-danger"){
-                    this.fundo_bg="bg-light animate__animated"
+                if(this.fundo_bg=="bg-danger text-center border rounded mb-5 p-2"){
+                    this.fundo_bg="bg-light text-center border rounded mb-5 p-2"
                     this.texto_bg="text-danger"
                 }else{
-                    this.fundo_bg="bg-danger"
+                    this.fundo_bg="bg-danger text-center border rounded mb-5 p-2"
                     this.texto_bg="text-light"
                 }
             }else{
                 this.$refs.gage.emit_alerta(false)
-                this.fundo_bg="bg-light"
+                this.fundo_bg="bg-light text-center border rounded mb-5 p-2"
                 this.texto_bg="text-success"
             }
         },
